9eefdd2b3f2e1e9c1cdf43477021f2e5f521212e
gaoxin-jfrogchina <31504568+xingao0803@users.noreply.github.com>
2018-12-13 14:42:58 +0800

    Update conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,7 +9,7 @@ class HelloTestConan(ConanFile):
     requires = "Hello/1.0@jfrog/stable"
 
     def build(self):
-        if settings.arch_build == "x86_64":
+        if arch == "x86_64":
             self.run("g++ ../../example.cpp @conanbuildinfo.args -o example")
         else :
             self.run("g++ m32 ../../example.cpp @conanbuildinfo.args -o example")
